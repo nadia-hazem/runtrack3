b9a1cdc814b22d3d70928c450ab2184a1951de4b
nedh <nedh.design@gmail.com>
2023-01-16 11:09:32 +0100

    jour01 - job02, job03 done !

---
+++ jour01/job02/script.js
@@ -0,0 +1,2 @@
+        // Dans la balise head, ajoutez une ligne de code javascript permettant d’afficher une popup qui contient le texte “Hello Javascript!”.
+        alert("Hello Javascript!");
---
+++ jour01/job03/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jour01 - Job03</title>
+    <script src="script.js"></script>
+    <!--Récupérez une copie de vos fichiers index.php et script.js. Modifier l’affichage de sorte
+à ce que le message ne soit plus écrit dans une popup mais dans la console web.
+Assurez-vous que tout est bien fonctionnel.-->
+    <script>
+        console.log("Hello Javascript!");
+    </script>
+    
+</head>
+<body>
+    
+</body>
+</html>

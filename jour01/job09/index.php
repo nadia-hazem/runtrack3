04ef93cfe466717f80e92513130508da31028346
nedh <nedh.design@gmail.com>
2023-01-16 19:38:59 +0100

    Amélioration du job09 jour01, ajout frontend

--- jour01/job09/index.php
+++ jour01/job09/index.php
@@ -7,12 +7,77 @@
     <title>Jour01 - Job09 - </title>
     <!-- Appeler le fichier script.js dans la balise head. -->
     <script src="script.js"></script>
+    <style>
+        body {
+            background-color: #f2f2f2;
+        }
+        .container {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+        }
+        form {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+        }
+        form label {
+            margin: 10px;
+        }
+        form input {
+            margin: 10px;
+        }
+        form input[type="button"] {
+            width: 120px;
+            height: 30px;
+            background-color: #4CAF50;
+            color: white;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+        form input[type="button"]:hover {
+            background-color: #45a049;
+        }
+        #result, #random {
+            margin: 10px;
+            text-align: center;
+        }
+        .set {
+            border: 1px solid black;
+            width: 200px;
+            min-height: 20px;
+            padding: 0.5rem;
+        }
+    </style>
     <!-- Créez une fonction “tri” qui prend en paramètres un tableau de nombres nommé
 “numbers” et une variable “order” qui contient “asc” ou “desc”. A l’aide de la fonction
 sort() d’un algorithme développé par vos soins, cette fonction doit trier le tableau dans
 l’ordre ascendant ou décroissant, selon le paramètre passé, puis retourner le tableau. -->
 </head>
 <body>
-    
+    <div class="container">
+        <form>
+            <h1 for="order">Ordre de tri </h1>
+            <select id="order">
+                <option value="asc">Ascendant</option>
+                <option value="desc">Descendant</option>
+            </select>
+            <br>
+            <input type="button" value="Générer et Trier" onClick="onFormSubmit()">
+        </form>
+        <p>Liste aléatoire</p>
+        <div class="set">
+            <span id="random"></span>
+        </div>
+
+        <p>Liste triée</p>
+        <div class="set">
+            <span id="result"></span>
+        </div>
+    </div>
 </body>
 </html>
--- jour01/job09/script.js
+++ jour01/job09/script.js
@@ -2,19 +2,39 @@
 // //////////////////////////////
 
 // Déclarez une fonction “tri” qui prend en paramètres un tableau de nombres nommé “numbers” et une variable “order” qui contient “asc” ou “desc”.
+
+// Génère une suite de 9 chiffres aléatoires
+function generateRandomNumbers() {
+    // On déclare un tableau vide
+    let numbers = [];
+    // On génère 9 nombres aléatoires entre 0 et 100
+    for (let i = 0; i < 9; i++) {
+        numbers.push(Math.floor(Math.random() * 100));
+    }
+    // On affiche le tableau dans la console et le html
+    console.log("Liste générée :");
+    console.log(numbers);
+    return numbers;
+}
+
 function tri(numbers, order) {
     // A l’aide de la fonction sort() d’un algorithme développé par vos soins, cette fonction doit trier le tableau dans l’ordre ascendant ou décroissant, selon le paramètre passé, puis retourner le tableau.
+
     if (order == "asc") {
+        // On utilise une boucle for imbriquée pour parcourir le tableau
         for (let i = 0; i < numbers.length; i++) {
             for (let j = 0; j < numbers.length; j++) {
                 if (numbers[i] < numbers[j]) {
+                    // On utilise une variable temporaire pour échanger les valeurs
                     let temp = numbers[i];
+                    // On échange les valeurs
                     numbers[i] = numbers[j];
                     numbers[j] = temp;
                 }
             }
         }
     } else if (order == "desc") {
+        // On utilise une boucle for imbriquée pour parcourir le tableau
         for (let i = 0; i < numbers.length; i++) {
             for (let j = 0; j < numbers.length; j++) {
                 if (numbers[i] > numbers[j]) {
@@ -25,11 +45,29 @@ function tri(numbers, order) {
             }
         }
     }
-    return console.log(numbers);
+    console.log("Liste triée :");
+    console.log(numbers);
+    return numbers;
 }
 
+// Fonction appelée lorsque le formulaire est soumis
+function onFormSubmit() {
+    // On récupère les nombres aléatoires
+    let numbers = generateRandomNumbers();
+    // On affiche les nombres aléatoires dans le html
+    document.getElementById("random").innerHTML = numbers;
+    // On récupère l'ordre de tri
+    let order = document.getElementById("order").value;
+    // On affiche l'ordre de tri dans le html
+    let sortedNumbers = tri(numbers, order);
+    // On affiche les nombres triés dans le html
+    document.getElementById("result").innerHTML = sortedNumbers;
+}
+
+
+
 // Appeler la fonction “tri”.
-tri([5, 4, 3, 2, 1], "asc");
+/* tri([5, 4, 3, 2, 1], "asc");
 tri([5, 4, 3, 2, 1], "desc");
 tri([8, 4, 9, 3, 5, 1, 2, 7, 6], "asc");
-tri([8, 4, 9, 3, 5, 1, 2, 7, 6], "desc");
+tri([8, 4, 9, 3, 5, 1, 2, 7, 6], "desc"); */

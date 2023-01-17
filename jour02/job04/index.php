f575138ffd3bcad855fcd274706588071a11b473
nedh <nedh.design@gmail.com>
2023-01-17 18:44:23 +0100

    jour02 - job04 done !

--- jour02/job04/index.php
+++ jour02/job04/index.php
@@ -6,6 +6,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Jour02 - Job04 - Keylogger</title>
     <script src="script.js"></script>
+    <style>
+        body {background-color: #f5f5f5;}
+        .container {
+            width: 100%; 
+            height: 100vh; 
+            display: flex; 
+            justify-content: center; 
+            align-items: center; 
+            flex-direction: column;
+        }
+        textarea:focus {
+            background-color: aliceblue;
+            border: 1px solid #f5f5f5;
+        }
+    </style>
 </head>
 <body>
     <div class="container">
@@ -13,7 +28,7 @@
         <div class="col">
             <h1 id="Keylogger"></h1>
             <div class="set">
-                <textarea id="textarea" cols="30" rows="10"></textarea>
+                <textarea id="keylogger" cols="50" rows="5"></textarea>
             </div>
         </div>
     </div>
--- jour02/job04/script.js
+++ jour02/job04/script.js
@@ -5,3 +5,26 @@
 // Chaque fois que l’utilisateur tape une lettre sur son clavier (a-z), celle-ci est ajoutée dans le textarea (même si le focus en cours n’est pas le textarea).
 // Si le focus en cours est dans le textarea, la lettre doit être ajoutée deux fois.
 
+// DOMContentLoaded pour éviter erreur de lancement de script avant chargement du DOM
+document.addEventListener("DOMContentLoaded", function() {
+
+    document.addEventListener("keydown", function(event) {
+        // Déclaration de la variable key qui prend la valeur de la touche pressée
+        let key = event.key;
+        // Si la touche pressée est comprise entre a et z
+        if (key >= 'a' && key <= 'z') {
+        // Déclaration de la variable textarea qui prend la valeur de l'élément textarea
+        let textarea = document.getElementById("keylogger");
+        // Si l'élément actif est le textarea
+        if (document.activeElement === textarea) {
+            // Ajout de la lettre deux fois
+            textarea.value += key;
+        }
+        // Ajout de la lettre une fois
+        textarea.value += key;
+        // Empêche le comportement par défaut de la touche pressée
+        event.preventDefault();
+        }
+    });
+
+});

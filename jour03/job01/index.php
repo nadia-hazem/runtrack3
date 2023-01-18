9d5af3a3caf0c6d58064cbb3809c0d4c169f4002
nedh <nedh.design@gmail.com>
2023-01-18 13:56:29 +0100

    Jour03 - Job01 done !

--- jour03/job01/index.php
+++ jour03/job01/index.php
@@ -5,9 +5,25 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Jour03 - Job01 - Bouton</title>
+    <!-- Bibliothèque jQuery -->
+    <script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
+    <!-- Script JS-->
+    <script src="script.js"></script>
+    <style>
+        #textContainer {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+
+    </style>
 </head>
 <body>
     <div class="container">
-        <button id="button">Cliquez ici</button>
+        <div id="textContainer">
+        <p>Les logiciels et les cathédrales, c'est un peu la même chose - d'abord on les construit, ensuite on prie.</p>
+    </div>
 </body>
 </html>
--- jour03/job01/script.js
+++ jour03/job01/script.js
@@ -5,3 +5,78 @@
 // “Les logiciels et les cathédrales, c'est un peu la même chose - d'abord on les construit, ensuite on prie.”
 // Ensuite créez un bouton qui servira à cacher tout l’élément html.
 
+$(document).ready(function() {
+    // On cache le texte
+    $('#textContainer p').hide();
+    // Variable qui va contenir l'état des boutons
+    let buttonsShown = "hide"; 
+
+    // fonction reset qui va permettre de réinitialiser les boutons
+    function reset(){
+        if(buttonsShown == "show"){
+            // Si le bouton show est affiché, on le supprime
+            $('#showTextBtn').remove();
+            // On crée le bouton hide
+            $('#textContainer').append('<button id="hideTextBtn">Cacher le texte</button>');
+            // On lui ajoute un écouteur d'événement
+            $('#hideTextBtn').click(function() {
+                // On cache le texte
+                $('#textContainer p').hide();
+                // On supprime le bouton
+                $('#hideTextBtn').remove();
+                // On change la valeur de la variable
+                buttonsShown = "hide";
+                // On réinitialise
+                reset();
+            });
+        } else {
+            // Si le bouton hide est affiché, on le supprime
+            $('#hideTextBtn').remove();
+            // On crée le bouton show
+            $('#textContainer').append('<button id="showTextBtn">Afficher le texte</button>');
+            // On lui ajoute un écouteur d'événement
+            $('#showTextBtn').click(function() {
+                // On ajoute le texte
+                $('#textContainer p').show();
+                // On supprime le bouton
+                $('#showTextBtn').remove();
+                // On change la valeur de la variable
+                buttonsShown = "show";
+                // On réinitialise
+                reset();
+            });
+        }
+    }
+    reset();
+});
+
+
+/* $('#showTextBtn').click(function() {
+    $('#textContainer p').show();
+    $('#showTextBtn').remove();
+    reset();
+}); */
+
+/*  $('#textContainer p').hide();
+    // On crée le bouton
+    $('#textContainer').append('<button id="showTextBtn">Afficher le texte</button>');
+    // On lui ajoute un écouteur d'événement
+    $('#showTextBtn').click(function() {
+    // On ajoute le texte
+    $('#textContainer').append("<p>Les logiciels et les cathédrales, c'est un peu la même chose - d'abord on les construit, ensuite on prie.</p>");
+    // On supprime le bouton
+    $('#showTextBtn').remove();
+    });
+    // On crée le bouton hide
+    $('#textContainer').append('<button id="hideTextBtn">Cacher le texte</button>');
+    // On lui ajoute un écouteur d'événement
+    $('#hideTextBtn').click(function() {
+    // On cache le texte
+    $('#textContainer p').hide();
+    // On supprime le bouton
+    $('#hideTextBtn').remove();
+    });
+}); */
+
+
+/* }); */

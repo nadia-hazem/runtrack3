2a590ed4b94ca8312662cd3c476fdc334ac557e0
nedh <nedh.design@gmail.com>
2023-01-18 04:35:49 +0100

    amélioration jour02 -  job06

--- jour02/job06/script.js
+++ jour02/job06/script.js
@@ -35,17 +35,17 @@ document.addEventListener("DOMContentLoaded", function() {
     body.style.fontFamily = "Roboto";
     body.style.display = "flex";
     body.style.flexDirection = "column";
-    /* body.style.justifyContent = "center";
-    body.style.alignItems = "center"; */
+    body.style.justifyContent = "center";
+    display.style.alignItems = "center";
+    body.style.alignItems = "strech !important";
     body.style.fontSize = "2em";
     body.style.fontWeight = "bold";
-    body.style.textShadow = "0 0 10px #fff";
     body.style.textTransform = "uppercase";
     body.style.textAlign = "center";
     body.style.letterSpacing = "0.2em";
     body.style.lineHeight = "1.2em";
     h1 = document.createElement("h1");
-    h1.style.cssText = "color: #000; font-size: 2em; font-weight: bold; text-shadow: 0 0 10px #fff; text-transform: uppercase; letter-spacing: 0.2em; line-height: 1.2em;";
+    h1.style.cssText = "color: #000; font-size: 2em; font-weight: bold; text-transform: uppercase; letter-spacing: 0.2em; line-height: 1.2em;";
     h1.innerHTML = "Fonction 'Konami' ";
     display.appendChild(h1);
     h2 = document.createElement("h2");
@@ -53,7 +53,7 @@ document.addEventListener("DOMContentLoaded", function() {
     h2.innerHTML = "Le code Konami (コナミコマンド, Konami, litt. Commande Konami) est un cheat code (code de triche) utilisé pour activer généralement une option secrète. Il est généralement entré en appuyant sur une séquence spécifique de touches :";
     display.appendChild(h2);
     img = document.createElement("img");
-    img.src = "konami.png";
+    img.src = "img/konami.png";
     img.style.cssText = "width: 400px; height: auto; margin-top: 2rem;";
     display.appendChild(img);
     p = document.createElement("p");
@@ -68,64 +68,56 @@ document.addEventListener("DOMContentLoaded", function() {
             konamiIndex++;
             if (konamiIndex === konami.length) {
                 // Konami saisi avec succès
-                // On change le style du body
-                display.style.display = "none";
-                body.style.backgroundColor = "#fff";
-                body.style.height = "100vh";
-                body.style.width = "100vw";
-                body.style.color = "#000";
-                body.style.fontFamily = "Roboto";
-                body.style.display = "flex";
-                body.style.flexDirection = "column";
-                body.style.justifyContent = "strech";
-                body.style.alignItems = "strech";
-                body.style.textAlign = "strech";
-                body.style.fontSize = "2em";
-                body.style.fontWeight = "bold";
-                body.style.textShadow = "0 0 10px #fff";
-                body.style.textTransform = "uppercase";
-                body.style.letterSpacing = "0.2em";
-                body.style.lineHeight = "1.2em";
+                // On cache le display
+                display.style.display = "none";             
                 
                 // Premier rang
                 const firstRow = document.createElement("div");
                 firstRow.style.display = "flex";
                 firstRow.style.flexDirection = "row";
 
-                // Première colonne body
+                // Première colonne du rang 1
                 const col1 = document.createElement("div");
                 col1.style.flex = 1;
                 col1.style.cssText = "position: relative; width: 50%; height: 100%;";
                 justifyContent = "center";
                 alignItems = "strech";
                 // Ajouter image
-                const img = document.createElement("img");
-                img.src = "plateforme.jpg";
-                img.style.cssText = "width: 100%; height: 100%; left: 0;";
-                col1.appendChild(img);
-                // Deuxième colonne body
+                const img2 = document.createElement("img");
+                img2.src = "img/plateforme.jpg";
+                img2.style.cssText = "width: 100%; height: 100%; left: 0;";
+                col1.appendChild(img2);
+                // Deuxième colonne du rang 1
                 const col2 = document.createElement("div");
                 col2.style.flex = 1;
                 col2.style.width = "50%";
-                col2.style.height = "100%";
+                col2.style.height = "auto";
+                col2.style.backgroundColor = "#336699";
+                col2.style.color = "#fff";
+                col2.style.paddingTop = "20%";
                 col2.justifyContent = "center";
                 col2.alignItems = "center";
+                col2.alignContent = "strech";
                 // Ajouter texte deuxième colonne
                 h1 = document.createElement("h1");
-                h1.style.cssText = "color: #000; font-size: 1em; font-weight: bold; text-shadow: 0 0 10px #fff; text-transform: uppercase; letter-spacing: 0.2em; line-height: 1.2em; margin: 3em 0 0 0;";
+                h1.style.cssText = "font-size: 1.5em; font-weight: bold; text-transform: uppercase; letter-spacing: 0.2em; line-height: 1.2em; margin: auto;";
                 h1.innerHTML = "La Plateforme_";
                 col2.appendChild(h1);
                 h2 = document.createElement("h2");
-                h2.style.cssText = "color: #000; font-size: 0.7em; font-weight: bold; text-shadow: 0 0 10px #fff; text-transform: uppercase; letter-spacing: 0.2em; line-height: 1.2em;";
+                h2.style.cssText = "color: #fff; font-size: 0.7em; font-weight: bold; text-transform: uppercase; letter-spacing: 0.2em; line-height: 1.2em;";
                 h2.innerHTML = "Campus Numérique Méditerranéen";
                 col2.appendChild(h2);
+                const img3 = document.createElement("img");
+                img3.src = "img/logoplateforme.svg";
+                img3.style.cssText = "width: 200px; height: auto; margin-top: 2rem;";
+                col2.appendChild(img3);
                 // Ajouter les colonnes au premier rang
                 firstRow.appendChild(col1);
                 firstRow.appendChild(col2);
                 // second rang
                 const secondRow = document.createElement("div");
                 const text = document.createElement("p");
-                text.innerHTML = "Your text here";
+                text.innerHTML = "Hé oui, encore un fake !";
                 secondRow.appendChild(text);
                 // Ajouter les rangs au body
                 body.appendChild(firstRow);

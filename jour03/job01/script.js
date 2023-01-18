e61da01e31fac7360b73b507c9df2a32de604392
nedh <nedh.design@gmail.com>
2023-01-18 12:43:31 +0100

    création fichiers & dossiers

---
+++ jour03/job01/script.js
@@ -0,0 +1,7 @@
+// Jour03 - Job01 - button appear/desappear
+
+// Créez une balise <button> en JQuery et sélectionnez-la dans votre code JavaScript.
+// En cliquant sur le bouton, vous devrez faire apparaître le texte sur votre page HTML :
+// “Les logiciels et les cathédrales, c'est un peu la même chose - d'abord on les construit, ensuite on prie.”
+// Ensuite créez un bouton qui servira à cacher tout l’élément html.
+

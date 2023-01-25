65f9be53f7b37f45b96bf40d264b35fe642d81ab
nedh <nedh.design@gmail.com>
2023-01-25 22:58:16 +0100

    ajout index à Runtrack3 pour accéder aux différents jobs

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Runtrack3 - Javascript</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Runtrack3 - Javascript</h1>
+    <div class="container">
+        <div class="jobs">
+            <h3>Jour01</h3>
+            <a href="jour01/job01/index.php">Job01</a>
+            <a href="jour01/job02/index.php">Job02</a>
+            <a href="jour01/job03/index.php">Job03</a>
+            <a href="jour01/job04/index.php">Job04</a>
+            <a href="jour01/job05/index.php">Job05</a>
+            <a href="jour01/job06/index.php">Job06</a>
+            <a href="jour01/job07/index.php">Job07</a>
+            <a href="jour01/job08/index.php">Job08</a>
+            <a href="jour01/job09/index.php">Job09</a>
+        </div>
+        <div class="jobs">
+            <h3>Jour02</h3>
+            <a href="jour02/job01/index.php">Job01</a>
+            <a href="jour02/job02/index.php">Job02</a>
+            <a href="jour02/job03/index.php">Job03</a>
+            <a href="jour02/job04/index.php">Job04</a>
+            <a href="jour02/job05/index.php">Job05</a>
+            <a href="jour02/job06/index.php">Job06</a>
+        </div>
+        <div class="jobs">
+            <h3>Jour03</h3>
+            <a href="jour03/job01/index.php">Job01</a>
+            <a href="jour03/job02/index.php">Job02</a>
+            <a href="jour03/job03/index.php">Job03</a>
+        </div>
+        <div class="jobs">
+            <h3>Jour04</h3>
+            <a href="jour04/job01/index.php">Job01</a>
+            <a href="jour04/job02/index.php">Job02</a>
+            <a href="jour04/job03/index.php">Job03</a>
+            <a href="jour04/job04/index.php">Job04</a>
+        </div>
+        <div class="jobs">
+            <h3>Jour05</h3>
+            <a href="jour05/job01/index.php">Job01</a>
+        </div>
+        <div class="jobs">
+            <h3>Jour06</h3>
+            <a href="jour06/job01/index.php">Job01</a>
+            <a href="jour06/job02/index.php">Job02</a>
+            <a href="jour06/job03/index.php">Job03</a>
+            <a href="jour06/job04/index.php">Job04</a>
+        </div>
+        <div class="jobs">
+            <h3>Jour07</h3>
+            <a href="jour07/job01/index.php">Job01</a>
+            <a href="jour07/job02/index.php">Job02</a>
+            <a href="jour07/job03/index.php">Job03</a>
+            <a href="jour07/job04/index.php">Job04</a>
+        </div>
+    </div>
+
+</body>
+</html>

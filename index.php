1c02e69127f8bf5faf871da78ccd395b3b5139a1
nedh <nedh.design@gmail.com>
2023-01-26 01:38:15 +0100

    css page index de tous les jobs

--- index.php
+++ index.php
@@ -7,60 +7,67 @@
     <title>Runtrack3 - Javascript</title>
     <link rel="stylesheet" href="style.css">
 </head>
-<body>
+<body id="runtrack3">
     <h1>Runtrack3 - Javascript</h1>
-    <div class="container">
-        <div class="jobs">
+    <div class="container wrap">
+        <div class="col">
             <h3>Jour01</h3>
-            <a href="jour01/job01/index.php">Job01</a>
-            <a href="jour01/job02/index.php">Job02</a>
-            <a href="jour01/job03/index.php">Job03</a>
-            <a href="jour01/job04/index.php">Job04</a>
-            <a href="jour01/job05/index.php">Job05</a>
-            <a href="jour01/job06/index.php">Job06</a>
-            <a href="jour01/job07/index.php">Job07</a>
-            <a href="jour01/job08/index.php">Job08</a>
-            <a href="jour01/job09/index.php">Job09</a>
+            <p>Hello JS</p>
+            <a class="card" href="jour01/job01/index.php" target="_blank">Job01</a>
+            <a class="card" href="jour01/job02/index.php" target="_blank">Job02</a>
+            <a class="card" href="jour01/job03/index.php" target="_blank">Job03</a>
+            <a class="card" href="jour01/job04/index.php" target="_blank">Job04</a>
+            <a class="card" href="jour01/job05/index.php" target="_blank">Job05</a>
+            <a class="card" href="jour01/job06/index.php" target="_blank">Job06</a>
+            <a class="card" href="jour01/job07/index.php" target="_blank">Job07</a>
+            <a class="card" href="jour01/job08/index.php" target="_blank">Job08</a>
+            <a class="card" href="jour01/job09/index.php" target="_blank">Job09</a>
         </div>
-        <div class="jobs">
+        <div class="col">
             <h3>Jour02</h3>
-            <a href="jour02/job01/index.php">Job01</a>
-            <a href="jour02/job02/index.php">Job02</a>
-            <a href="jour02/job03/index.php">Job03</a>
-            <a href="jour02/job04/index.php">Job04</a>
-            <a href="jour02/job05/index.php">Job05</a>
-            <a href="jour02/job06/index.php">Job06</a>
+            <p>JS ++</p>
+            <a class="card" href="jour02/job01/index.php" target="_blank">Job01</a>
+            <a class="card" href="jour02/job02/index.php" target="_blank">Job02</a>
+            <a class="card" href="jour02/job03/index.php" target="_blank">Job03</a>
+            <a class="card" href="jour02/job04/index.php" target="_blank">Job04</a>
+            <a class="card" href="jour02/job05/index.php" target="_blank">Job05</a>
+            <a class="card" href="jour02/job06/index.php" target="_blank">Job06</a>
         </div>
-        <div class="jobs">
+        <div class="col">
             <h3>Jour03</h3>
-            <a href="jour03/job01/index.php">Job01</a>
-            <a href="jour03/job02/index.php">Job02</a>
-            <a href="jour03/job03/index.php">Job03</a>
+            <p>jQuery</p>
+            <a class="card" href="jour03/job01/index.php" target="_blank">Job01</a>
+            <a class="card" href="jour03/job02/index.php" target="_blank">Job02</a>
+            <a class="card" href="jour03/job03/index.php" target="_blank">Job03</a>
         </div>
-        <div class="jobs">
+        <div class="col">
             <h3>Jour04</h3>
-            <a href="jour04/job01/index.php">Job01</a>
-            <a href="jour04/job02/index.php">Job02</a>
-            <a href="jour04/job03/index.php">Job03</a>
-            <a href="jour04/job04/index.php">Job04</a>
+            <p>Fetch</p>
+            <a class="card" href="jour04/job01/index.php" target="_blank">Job01</a>
+            <a class="card" href="jour04/job02/index.php" target="_blank">Job02</a>
+            <a class="card" href="jour04/job03/index.php" target="_blank">Job03</a>
+            <a class="card" href="jour04/job04/index.php" target="_blank">Job04</a>
         </div>
-        <div class="jobs">
+        <div class="col">
             <h3>Jour05</h3>
-            <a href="jour05/job01/index.php">Job01</a>
+            <p>Projet</p>
+            <a class="card" href="jour05/job01/index.php" target="_blank">Job01</a>
         </div>
-        <div class="jobs">
+        <div class="col">
             <h3>Jour06</h3>
-            <a href="jour06/job01/index.php">Job01</a>
-            <a href="jour06/job02/index.php">Job02</a>
-            <a href="jour06/job03/index.php">Job03</a>
-            <a href="jour06/job04/index.php">Job04</a>
+            <p>Media Queries</p>
+            <a class="card" href="jour06/job01/index.php" target="_blank">Job01</a>
+            <a class="card" href="jour06/job02/index.php" target="_blank">Job02</a>
+            <a class="card" href="jour06/job03/index.php" target="_blank">Job03</a>
+            <a class="card" href="jour06/job04/index.php" target="_blank">Job04</a>
         </div>
-        <div class="jobs">
+        <div class="col">
             <h3>Jour07</h3>
-            <a href="jour07/job01/index.php">Job01</a>
-            <a href="jour07/job02/index.php">Job02</a>
-            <a href="jour07/job03/index.php">Job03</a>
-            <a href="jour07/job04/index.php">Job04</a>
+            <p>Tailwind</p>
+            <a class="card" href="jour07/job01/index.php" target="_blank">Job01</a>
+            <a class="card" href="jour07/job02/index.php" target="_blank">Job02</a>
+            <a class="card" href="jour07/job03/index.php" target="_blank">Job03</a>
+            <a class="card" href="jour07/job04/index.php" target="_blank">Job04</a>
         </div>
     </div>
 
